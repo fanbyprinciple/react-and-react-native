882b054ff5dce1abd09a1474777b2ef3d5dd3d0b
fanbyprinciple <fanbyprinciple@gmail.com>
2025-03-20 23:55:06 +0530

    nothings new

--- chessonly/App.js
+++ chessonly/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Chessboard from 'react-native-chessboard';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Worlds</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
